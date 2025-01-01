b0dd3d9e87536611d520633d047b91498861f2e8
Nkemdilim Collins <collinsnkemdilim05@gmail.com>
2025-01-01 10:36:40 +0100

    finished navbar

---
+++ main.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.hamburger');
+const firstLine = document.querySelector('.span-1');
+const secondLine = document.querySelector('.span-2');
+const thirdLine = document.querySelector('.span-3');
+const links = document.querySelector('.links');
+
+let clicked = false;
+
+hamburger.addEventListener('click', () => {
+  if (!clicked) {
+    firstLine.classList.add('-rotate-45');
+    firstLine.classList.add('translate-y-[22px]');
+    secondLine.classList.add('invisible');
+    thirdLine.classList.add('rotate-45');
+    links.classList.remove('invisible');
+    clicked = true;
+  } else {
+    firstLine.classList.remove('-rotate-45');
+    firstLine.classList.remove('translate-y-[22px]');
+    secondLine.classList.remove('invisible');
+    thirdLine.classList.remove('rotate-45');
+    links.classList.add('invisible');
+    clicked = false;
+  }
+});
